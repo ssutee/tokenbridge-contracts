b653136e0a37cacb72574406c13a03dd8bc5cac5
Sutee Sudprasert <sutee.s@gmail.com>
2023-03-12 08:27:49 +0700

    unlock withdrawing bridge token

--- contracts/upgradeable_contracts/amb_erc677_to_erc677/ForeignAMBErc677ToErc677.sol
+++ contracts/upgradeable_contracts/amb_erc677_to_erc677/ForeignAMBErc677ToErc677.sol
@@ -119,7 +119,7 @@ contract ForeignAMBErc677ToErc677 is BasicAMBErc677ToErc677, MediatorBalanceStor
     * @param _to address that will receive the locked tokens on this contract.
     */
     function claimTokens(address _token, address _to) external onlyIfUpgradeabilityOwner {
-        require(_token != address(_erc677token()));
+        // require(_token != address(_erc677token()));
         claimValues(_token, _to);
     }
 }
